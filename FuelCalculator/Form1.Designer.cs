7bcc516c23e6c315fcfcfd9bab074523a4d1c565
Ryan Steinwert <ryansteinwert@gmail.com>
2021-12-14 23:12:08 -0800

    Initial commit with form setup

---
+++ FuelCalculator/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace FuelCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.raceTimeLabel = new System.Windows.Forms.Label();
+            this.lapTimeLabel = new System.Windows.Forms.Label();
+            this.lapTimeInput = new System.Windows.Forms.TextBox();
+            this.maxFuelLabel = new System.Windows.Forms.Label();
+            this.usageLabel = new System.Windows.Forms.Label();
+            this.raceTimeInput = new System.Windows.Forms.NumericUpDown();
+            this.maxFuelInput = new System.Windows.Forms.NumericUpDown();
+            this.usageInput = new System.Windows.Forms.NumericUpDown();
+            this.lapRaceCheckbox = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.extraLapsSlider = new System.Windows.Forms.TrackBar();
+            this.extraLapsLabel = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.raceTimeInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxFuelInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usageInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extraLapsSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // raceTimeLabel
+            // 
+            this.raceTimeLabel.AutoSize = true;
+            this.raceTimeLabel.Location = new System.Drawing.Point(13, 13);
+            this.raceTimeLabel.Name = "raceTimeLabel";
+            this.raceTimeLabel.Size = new System.Drawing.Size(110, 13);
+            this.raceTimeLabel.TabIndex = 1;
+            this.raceTimeLabel.Text = "Race Time in Minutes";
+            // 
+            // lapTimeLabel
+            // 
+            this.lapTimeLabel.AutoSize = true;
+            this.lapTimeLabel.Location = new System.Drawing.Point(13, 56);
+            this.lapTimeLabel.Name = "lapTimeLabel";
+            this.lapTimeLabel.Size = new System.Drawing.Size(151, 13);
+            this.lapTimeLabel.TabIndex = 2;
+            this.lapTimeLabel.Text = "Lap Time in Format MM:SS.ms";
+            // 
+            // lapTimeInput
+            // 
+            this.lapTimeInput.Location = new System.Drawing.Point(16, 72);
+            this.lapTimeInput.Name = "lapTimeInput";
+            this.lapTimeInput.Size = new System.Drawing.Size(218, 20);
+            this.lapTimeInput.TabIndex = 3;
+            // 
+            // maxFuelLabel
+            // 
+            this.maxFuelLabel.AutoSize = true;
+            this.maxFuelLabel.Location = new System.Drawing.Point(13, 101);
+            this.maxFuelLabel.Name = "maxFuelLabel";
+            this.maxFuelLabel.Size = new System.Drawing.Size(74, 13);
+            this.maxFuelLabel.TabIndex = 4;
+            this.maxFuelLabel.Text = "Maximum Fuel";
+            // 
+            // usageLabel
+            // 
+            this.usageLabel.AutoSize = true;
+            this.usageLabel.Location = new System.Drawing.Point(13, 144);
+            this.usageLabel.Name = "usageLabel";
+            this.usageLabel.Size = new System.Drawing.Size(61, 13);
+            this.usageLabel.TabIndex = 6;
+            this.usageLabel.Text = "Fuel Usage";
+            // 
+            // raceTimeInput
+            // 
+            this.raceTimeInput.Location = new System.Drawing.Point(16, 30);
+            this.raceTimeInput.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.raceTimeInput.Name = "raceTimeInput";
+            this.raceTimeInput.Size = new System.Drawing.Size(218, 20);
+            this.raceTimeInput.TabIndex = 8;
+            this.raceTimeInput.ValueChanged += new System.EventHandler(this.raceTimeInput_ValueChanged);
+            // 
+            // maxFuelInput
+            // 
+            this.maxFuelInput.DecimalPlaces = 2;
+            this.maxFuelInput.Location = new System.Drawing.Point(16, 118);
+            this.maxFuelInput.Name = "maxFuelInput";
+            this.maxFuelInput.Size = new System.Drawing.Size(218, 20);
+            this.maxFuelInput.TabIndex = 9;
+            // 
+            // usageInput
+            // 
+            this.usageInput.DecimalPlaces = 2;
+            this.usageInput.Location = new System.Drawing.Point(16, 161);
+            this.usageInput.Name = "usageInput";
+            this.usageInput.Size = new System.Drawing.Size(218, 20);
+            this.usageInput.TabIndex = 10;
+            // 
+            // lapRaceCheckbox
+            // 
+            this.lapRaceCheckbox.AutoSize = true;
+            this.lapRaceCheckbox.Location = new System.Drawing.Point(16, 305);
+            this.lapRaceCheckbox.Name = "lapRaceCheckbox";
+            this.lapRaceCheckbox.Size = new System.Drawing.Size(73, 17);
+            this.lapRaceCheckbox.TabIndex = 11;
+            this.lapRaceCheckbox.Text = "Lap Race";
+            this.lapRaceCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "No Pace Lap",
+            "Half Pace Lap",
+            "Full Pace Lap"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 205);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // extraLapsSlider
+            // 
+            this.extraLapsSlider.Location = new System.Drawing.Point(16, 254);
+            this.extraLapsSlider.Name = "extraLapsSlider";
+            this.extraLapsSlider.Size = new System.Drawing.Size(218, 45);
+            this.extraLapsSlider.TabIndex = 13;
+            // 
+            // extraLapsLabel
+            // 
+            this.extraLapsLabel.AutoSize = true;
+            this.extraLapsLabel.Location = new System.Drawing.Point(13, 238);
+            this.extraLapsLabel.Name = "extraLapsLabel";
+            this.extraLapsLabel.Size = new System.Drawing.Size(57, 13);
+            this.extraLapsLabel.TabIndex = 14;
+            this.extraLapsLabel.Text = "Extra Laps";
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(16, 346);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 15;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 400);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.extraLapsLabel);
+            this.Controls.Add(this.extraLapsSlider);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lapRaceCheckbox);
+            this.Controls.Add(this.usageInput);
+            this.Controls.Add(this.maxFuelInput);
+            this.Controls.Add(this.raceTimeInput);
+            this.Controls.Add(this.usageLabel);
+            this.Controls.Add(this.maxFuelLabel);
+            this.Controls.Add(this.lapTimeInput);
+            this.Controls.Add(this.lapTimeLabel);
+            this.Controls.Add(this.raceTimeLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.raceTimeInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxFuelInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usageInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extraLapsSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label raceTimeLabel;
+        private System.Windows.Forms.Label lapTimeLabel;
+        private System.Windows.Forms.TextBox lapTimeInput;
+        private System.Windows.Forms.Label maxFuelLabel;
+        private System.Windows.Forms.Label usageLabel;
+        private System.Windows.Forms.NumericUpDown raceTimeInput;
+        private System.Windows.Forms.NumericUpDown maxFuelInput;
+        private System.Windows.Forms.NumericUpDown usageInput;
+        private System.Windows.Forms.CheckBox lapRaceCheckbox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar extraLapsSlider;
+        private System.Windows.Forms.Label extraLapsLabel;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
